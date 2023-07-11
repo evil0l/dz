97137d1e4980e728173531ed7abcd9fc95246797
evil0l <139062839+evil0l@users.noreply.github.com>
2023-07-11 13:47:19 +0300

    Update test_our_site_3.py

--- test_our_site_3.py
+++ test_our_site_3.py
@@ -34,6 +34,8 @@ def element_click(xpath, driver):
     element.click()
 
 def login(login, password):
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()),
+                              options=chrome_options)
     element_send_keys(xpath="user-name", driver=driver, text=login)
     element_send_keys(xpath="password", driver=driver, text=password)
     element_click(xpath='login-button', driver=driver)
